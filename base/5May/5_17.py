c920a369192f4c4623383f0bcb28aec2b57270f8
GitHub-fly <1224178565@qq.com>
2020-05-17 01:48:39 +0800

    使用 ThreadPoolExecutor 向线程池进行任务提交者 练习

---
+++ base/5May/5_17.py
@@ -0,0 +1,22 @@
+"""
+使用 ThreadPoolExecutor 向线程池进行任务提交者
+更方便的从被调用函数中获取返回值
+"""
+from concurrent.futures import ThreadPoolExecutor
+import urllib.request
+
+
+def fetch_url(url):
+    u = urllib.request.urlopen(url)
+    data = u.read()
+    return data
+
+
+pool = ThreadPoolExecutor(10)
+# 提交到线程池
+a = pool.submit(fetch_url, 'http://www.python.org')
+b = pool.submit(fetch_url, 'http://www.pypy.org')
+
+# 获得的 handle 对象会处理所有的阻塞与写作，然后从工业线程中返回数据
+x = a.result()
+y = b.result()
